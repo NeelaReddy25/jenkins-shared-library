a55c515756b63325897c6479663eadb0b9df7afa
NeelaReddy <neelareddy.i25@gmail.com>
2024-07-19 12:36:54 +0530

    good effects

---
+++ vars/pipelineGlobals.groovy
@@ -0,0 +1,9 @@
+static final String region(){
+    return "us-east-1"
+}
+static final String nexusURL(){
+    return "nexus.neelareddy.store:8081"
+}
+static final String account_id(){
+    return "533267327306"
+}
